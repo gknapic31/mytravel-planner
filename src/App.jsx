a48627dc75b140e1e7139e66784ece99928b7e98
gknapic31 <95174747+gknapic31@users.noreply.github.com>
2025-04-25 00:27:56 +0200

    project setup and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button'
+import Hero from './components/ui/custom/Hero.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<>
+    <>
       {/*Hero*/}
       <Hero />
     </>
